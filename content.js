7ec1a88a221e00bf68912fd741bd79be42ece1ab
Nathan Bisson <nathan.g.bisson@gmail.com>
2018-11-18 14:45:29 -0500

    All prices for product now being written to page

--- content.js
+++ content.js
@@ -1,19 +1,27 @@
-var products = document.getElementsByClassName("btn--primary product-tile__cta");
+var products = document.getElementsByClassName("btn--primary product-tile__cta hidden-desktop");
+var counter = 0;
+for(var j = 0; j < products.length; j++) {
+	var productUrl = products[j].href;
 
-console.log(products[0].href);
-
-$.ajax({
-	type: 'GET',
-	url: products[0].href,
-	dataType: 'json',
-	success: function(response) {
-		console.log(response.product.variants);
-		var units = response.product.variants;
-		for (var i = 0; i < units.length; i++) {
-			console.log(units[i]);
+	$.ajax({
+		type: 'GET',
+		url: productUrl,
+		dataType: 'json',
+		success: function(response) {
+			var units = response.product.variants;
+			var productInfoString = "";
+			for (var i = 0; i < units.length; i++) {
+				var weight = units[i].title;
+				var price = units[i].price;
+				var unitString = weight + ': ' + price + ' ';
+				productInfoString += unitString;
+			}
+			var classTag = '.product-tile__price' + ':eq(' + counter + ')';
+			$(classTag).text(productInfoString);
+			counter++
+		},
+		error: function() {
+			console.log('Error Loading');
 		}
-	},
-	error: function() {
-		console.log('Error Loading');
-	}
-});
+	});
+}
